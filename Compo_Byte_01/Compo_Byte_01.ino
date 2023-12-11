c256076d33b9f6b313175bd1339d10c426e5541f
jlswbs <jlswbs@users.noreply.github.com>
2023-12-11 09:26:56 +0100

    Add files via upload

---
+++ Compo_Byte_01/Compo_Byte_01.ino
@@ -0,0 +1,63 @@
+// Composite video and Byte-Beat algo 01 //
+
+#include <M5Unified.h>
+#include <M5UnitRCA.h>
+
+#define WIDTH   256
+#define HEIGHT  128
+
+M5UnitRCA gfx_rca (WIDTH, HEIGHT, WIDTH, HEIGHT, M5UnitRCA::signal_type_t::PAL, M5UnitRCA::use_psram_t::psram_no_use, 26);
+
+  long t, cnt, col;
+  int16_t raw_data[WIDTH];
+
+void setup(void) {
+
+  M5.begin();  
+
+  auto spk_cfg        = M5.Speaker.config();
+  spk_cfg.i2s_port    = I2S_NUM_1;
+  spk_cfg.sample_rate = 48000;
+  spk_cfg.pin_data_out = 15;
+  spk_cfg.pin_bck      = 13;
+  spk_cfg.pin_ws       = 0;  // LRCK
+  spk_cfg.stereo        = true;
+  spk_cfg.buzzer        = false;
+  spk_cfg.use_dac       = false;
+  spk_cfg.magnification = 16;
+  M5.Speaker.config(spk_cfg);
+
+  M5.Speaker.begin();
+  M5.Speaker.setVolume(16);
+  M5.Speaker.setChannelVolume(0, 255);
+  M5.Speaker.setChannelVolume(1, 255);
+
+  gfx_rca.init();
+  gfx_rca.setOutputPin(26);
+
+  gfx_rca.clear(TFT_BLACK);
+    
+}
+
+void loop(void) {
+
+for (int y = 0; y < HEIGHT; ++y) {
+
+  for (int x = 0; x < WIDTH; ++x){
+
+      col = cnt + (t<<0^t>>10)-(t|t>>9);
+
+      gfx_rca.drawPixel((col+cnt)%WIDTH, y, (uint16_t)col);
+      raw_data[x] = (int16_t)col;
+
+      t = t + 128;
+
+    }
+
+    M5.Speaker.playRaw(raw_data, WIDTH, 48000, false, 1, -1, false);
+
+  }
+
+  cnt++;
+ 
+}
---
+++ Compo_Byte_03/Compo_Byte_03.ino
@@ -0,0 +1,65 @@
+// Composite video and Byte-Beat algo 03 //
+
+#include <M5Unified.h>
+#include <M5UnitRCA.h>
+
+#define WIDTH   256
+#define HEIGHT  128
+
+M5UnitRCA gfx_rca (WIDTH, HEIGHT, WIDTH, HEIGHT, M5UnitRCA::signal_type_t::PAL, M5UnitRCA::use_psram_t::psram_no_use, 26);
+
+  long t, cnt, col;
+  uint16_t coll;
+  int16_t raw_data[WIDTH];
+
+void setup(void) {
+
+  M5.begin();  
+
+  auto spk_cfg        = M5.Speaker.config();
+  spk_cfg.i2s_port    = I2S_NUM_1;
+  spk_cfg.sample_rate = 48000;
+  spk_cfg.pin_data_out = 15;
+  spk_cfg.pin_bck      = 13;
+  spk_cfg.pin_ws       = 0;  // LRCK
+  spk_cfg.stereo        = true;
+  spk_cfg.buzzer        = false;
+  spk_cfg.use_dac       = false;
+  spk_cfg.magnification = 16;
+  M5.Speaker.config(spk_cfg);
+
+  M5.Speaker.begin();
+  M5.Speaker.setVolume(16);
+  M5.Speaker.setChannelVolume(0, 255);
+  M5.Speaker.setChannelVolume(1, 255);
+
+  gfx_rca.init();
+  gfx_rca.setOutputPin(26);
+
+  gfx_rca.clear(TFT_BLACK);
+    
+}
+
+void loop(void) {
+
+for (int y = 0; y < HEIGHT; ++y) {
+
+  for (int x = 0; x < WIDTH; ++x){
+
+      col = 100*t & (x|t>>10) ^ (y|t>>9) + cnt;
+
+      gfx_rca.drawPixel((col+cnt)%WIDTH, y, coll);
+      raw_data[x] = (int16_t)col;
+
+      t = t + 64;
+
+    }
+
+    M5.Speaker.playRaw(raw_data, WIDTH, 48000, false, 1, -1, false);
+
+  }
+
+  coll = col;
+  cnt++;
+ 
+}
